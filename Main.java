7262ee7723585f64bd75f7f2aa6a37a5b68c96ca
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-11-13 19:48:44 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         int[] mainArray = {49, 15, 40, 27, 20, 19, 50, 2, 12, 28, 49, 44, 9, 59, 18, 5, 30, 6, 7, 28};
 
         System.out.println("\n - = Bubble Sort = - \n");
